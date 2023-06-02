d7d5fe0592ffcd99a726c3c4e9e094a2b8c6ee85
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-06-02 14:52:52 -0400

    Add Card Drag

---
+++ Assets/Scripts/UI/Backpack/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop on" + gameObject.name);
+        DraggableObj draggedObject = eventData.pointerDrag.GetComponent<DraggableObj>();
+        if (draggedObject == null) return;
+        draggedObject.ParentToReturnTo = this.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
